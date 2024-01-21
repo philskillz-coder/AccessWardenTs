44e58a191846852bbfc2c945fd952f04c91fb8eb
Philskillz <76884393+philskillz-coder@users.noreply.github.com>
2024-01-21 11:32:12 +0100

    fixed migration

--- src/backend/database/migration/1705175152800-migration.ts
+++ src/backend/database/migration/1705175152800-migration.ts
@@ -1,14 +1,14 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class Migration1705175152800 implements MigrationInterface {
-    name = 'Migration1705175152800'
+    name = "Migration1705175152800";
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "permission" ALTER COLUMN "description" SET DEFAULT ''`);
+        await queryRunner.query("ALTER TABLE \"permission\" ALTER COLUMN \"description\" SET DEFAULT ''");
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "permission" ALTER COLUMN "description" DROP DEFAULT`);
+        await queryRunner.query("ALTER TABLE \"permission\" ALTER COLUMN \"description\" DROP DEFAULT");
     }
 
 }
